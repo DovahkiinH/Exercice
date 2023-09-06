f60224f8bc0d214947dd2c7701082eee671fdd1a
DovahkiinH <loick.caplier@gmail.com>
2023-09-06 15:46:53 +0200

    JS

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -1,12 +1,12 @@
 var validation = document.getElementById("bouton_envoi");
-var prenom = document.getElementById("prenom");
+var prenom = document.getElementById("prénom");
 var prenom_m = document.getElementById("prenom_manquant");
 var prenom_v = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?/;
 validation.addEventListener("click", f_valid);
 
 function f_valid(e){
 
-    if (prenom.vaidity.valueMissing) {
+    if (prénom.vaidity.valueMissing) {
         e.preventDefault();
         prenom_m.textContent = "Prénom manquant";
         prenom_m.style.color = "red";
@@ -17,6 +17,6 @@ function f_valid(e){
         prenom_m.style.color = "orange";
 
     }else {
-        
+
     }
 }
